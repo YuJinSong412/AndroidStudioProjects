dcf044331a739d0967a5096581de0576ba0f9e37
yujin <yujin@naver.com>
2018-04-01 17:31:19 +0900

    Hello

---
+++ Hello/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -0,0 +1,19 @@
+package org.techtown.hello;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this,"버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
+    }
+}
