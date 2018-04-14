854aadc90706e98b7e93f1cfb49adb301d1a4343
yujin <yujin@naver.com>
2018-04-14 21:57:00 +0900

    add

--- Test2/app/src/main/java/org/techtown/test/MainActivity.java
+++ Test2/app/src/main/java/org/techtown/test/MainActivity.java
@@ -7,11 +7,10 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    
     ImageView imageView;
     ImageView imageView2;
-    
-    int index = 0;
+
+   int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +21,20 @@ public class MainActivity extends AppCompatActivity {
         imageView2 = (ImageView) findViewById(R.id.imageView2);
     }
 
-    public void onButtonClicked(View v){
-        index += 1;
+    public void onButton1Clicked(View v) {
+            index +=1;
 
-        if(index > 1){
-            index =0;
-        }
+            if(index > 1){
+                index =0;
+            }
 
-        if(index == 0){
-            imageView.setVisibility(View.VISIBLE);
-            imageView2.setVisibility(View.INVISIBLE);
+            if(index == 0){
+                imageView.setVisibility(View.VISIBLE);
+                imageView2.setVisibility(View.INVISIBLE);
+            }
+            else if(index == 1){
+                imageView.setVisibility(View.INVISIBLE);
+                imageView2.setVisibility(View.VISIBLE);
+            }
         }
-        else if(index == 1){
-            imageView.setVisibility(View.INVISIBLE);
-            imageView2.setVisibility(View.VISIBLE);
-        }
-    }
 }
