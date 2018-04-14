34f0e83a8fa872a511747371ac629a921f0cb47d
yujin <yujin@naver.com>
2018-04-14 23:51:36 +0900

    d

--- TTest/app/src/main/java/org/techtown/ttest/MainActivity.java
+++ TTest/app/src/main/java/org/techtown/ttest/MainActivity.java
@@ -1,38 +1,37 @@
-package org.techtown.test;
+package org.techtown.ttest;
 
 import android.content.Intent;
-import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
-    ImageView imageView2;
+    TextView textView;
 
     boolean index = true;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.imageView3);
-        imageView2 = (ImageView) findViewById(R.id.imageView4);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
     }
 
     public void onButton1Clicked(View v){
         if(index){
             imageView.setVisibility(View.VISIBLE);
-            imageView2.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.INVISIBLE);
             index = false;
         }
         else{
             imageView.setVisibility(View.INVISIBLE);
-            imageView2.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.VISIBLE);
             index = true;
         }
     }
@@ -43,8 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButton3Clicked(View v){
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://terms.naver.com"));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.terms.naver.com"));
         startActivity(intent);
     }
-
 }
